7ffa64838aac18dc9759078c33992b196a2d829b
lapadol <lapadol6109@gmail.com>
2020-08-28 15:55:48 +0700

    service example

--- ApiConsulTest/MVCAngular1/wwwroot/js/Services/MyService.js
+++ ApiConsulTest/MVCAngular1/wwwroot/js/Services/MyService.js
@@ -1,1 +1,48 @@
-﻿
+﻿var app = angular.module('myApp', []);
+app = app
+    .factory('MyService', function () {
+
+        // private
+        var value = 0;
+
+        // public
+        return {
+
+            getValue: function () {
+                return value;
+            },
+
+            setValue: function (val) {
+                value = val;
+            }
+
+        };
+    });
+
+app = app
+    .factory('MyService2', function () {
+
+        // private
+        var value = 0;
+
+        // public
+        return {
+
+            getValue: function () {
+                return value + "x";
+            },
+
+            setValue: function (val) {
+                value = val;
+            }
+
+        };
+    });
+
+app = app.service('SomeService', function () {
+    this.someFunction = function () { console.log("some function 1 is debug");};
+});
+
+app = app.service('SomeService1', function () {
+    this.someFunction = function () { console.log("some function 2 is debug");};
+});
